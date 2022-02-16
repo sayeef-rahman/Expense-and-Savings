0c5e2c2187f7cd3178d8ade4e7d48e0248e039c1
sayeef-rahman <sayeef.cse@gmail.com>
2022-02-17 01:25:39 +0600

    All calculation complete

--- js/app.js
+++ js/app.js
@@ -15,6 +15,11 @@ function inputBox(inputField){
         return inputValue;
     }
 }
+function totalExpense(food,rent,cloth){
+    const expense= food+rent+cloth;
+    return expense;
+}
+
 
 document.getElementById('calculate-btn').addEventListener('click',function(){
     const incomeTotal= inputBox('income-input');
@@ -26,20 +31,50 @@ document.getElementById('calculate-btn').addEventListener('click',function(){
     const clothesTotal= inputBox('clothes-input');
     // console.log('Total cloth: ',clothesTotal);
 
-    const expenseTotal= foodTotal+rentTotal+clothesTotal;
+    const expenseTotal=  totalExpense(foodTotal,rentTotal,clothesTotal);
     const balanceTotal= incomeTotal - expenseTotal;
     
     // console.log('Total Expense: ',expenseTotal);
 
     if(incomeTotal < expenseTotal){
-        return alert('Income Can Not be Less Than Expense')
+        const note= document.getElementById('last-note');
+        note.innerText='Income Can not be less than Expense';
+        // return alert('Income Can Not be Less Than Expense')
     }
     else{
         document.getElementById('total-expense').innerText = expenseTotal;
         document.getElementById('total-balance').innerText = balanceTotal;
     }
 
+});
 
+document.getElementById('save-btn').addEventListener('click',function(){
+    const incomeTotal= inputBox('income-input');
+    // console.log('Total Income: ',incomeTotal);
+    const foodTotal= inputBox('food-input');
+    // console.log('Total Food: ',foodTotal);
+    const rentTotal= inputBox('rent-input');
+    // console.log('Total rent: ',rentTotal);
+    const clothesTotal= inputBox('clothes-input');
+    // console.log('Total cloth: ',clothesTotal);
 
+    const expenseTotal=  totalExpense(foodTotal,rentTotal,clothesTotal);
+    const balanceTotal= incomeTotal - expenseTotal;
 
+    const savePercent= inputBox('save-input');
+    //console.log('Saving Percentage: ',savePercent);
+    const savingAmount= incomeTotal * (savePercent/100);
+    //console.log('saving amount: ',savingAmount);
+
+    if(balanceTotal > savingAmount){
+        document.getElementById('saving-amount').innerText = savingAmount;
+        const remainingBalance = balanceTotal - savingAmount;
+        document.getElementById('remaining-balance').innerText = remainingBalance;
+        console.log('remaining amount: ',remainingBalance);
+    }
+    else{
+        const note= document.getElementById('last-note');
+        note.innerText='Balance Can not be less than Savings';
+        // return alert('Balance Can not be less than savings')
+    }
 });
